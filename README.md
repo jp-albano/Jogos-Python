e42118699e9f3180fca6b55219cc010782f2d82d
João Paulo <jpalbano42@gmail.com>
2026-08-03 18:07:07 -0300

    primeiro commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# jogo-da-velha
+Projeto final do curso Python Essentials 1 da Cisco
+
+Primeiro commit para deixar as funções anotadas, e o começo do tabuleiro
---
+++ main.py
@@ -0,0 +1,27 @@
+from random import randrange
+
+def imprimirTabuleiro(tabuleiro):
+    for linha in tabuleiro:
+        print(linha)
+
+def enter_move(tabuleiro, jogada):
+    
+    for x, linha in range(3):
+        for y, jogada in range(3):
+            if 
+
+
+def make_list_of_free_fields(tabuleiro):
+
+def victory_for(tabuleiro, sign):
+
+def draw_move(tabuleiro):
+
+tabuleiro = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+imprimirTabuleiro(tabuleiro)
+
